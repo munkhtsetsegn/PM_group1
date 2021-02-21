484c1d6e7e760ceb59a8f0604aefe1b43646b60e
Amraa9656 <amarjargalamraa61@gmail.com>
2021-02-22 00:23:35 +0800

    18b1num2004

---
+++ Tosol/src/test/java/com/example/Tosol/TosolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tosol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
