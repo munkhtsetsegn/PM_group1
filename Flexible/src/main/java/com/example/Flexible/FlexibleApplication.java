6226fc9762711b2f849daf5874a4f0cd89b7ca58
covidslayer69 <xacap46@gmail.com>
2021-02-21 23:26:33 +0800

    Spring

---
+++ Flexible/src/main/java/com/example/Flexible/FlexibleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flexible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexibleApplication.class, args);
+	}
+
+}
---
+++ Flexible/src/test/java/com/example/Flexible/FlexibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Flexible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
