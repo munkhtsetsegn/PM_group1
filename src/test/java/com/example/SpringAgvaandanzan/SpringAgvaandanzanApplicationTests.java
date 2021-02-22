39e1ba952c0d058000677b84abf51055c517871a
Agvaandanzan <aagi.j@yahoo.com>
2021-02-22 12:46:00 +0800

    Environment baiguulaw

---
+++ src/test/java/com/example/SpringAgvaandanzan/SpringAgvaandanzanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAgvaandanzan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAgvaandanzanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
