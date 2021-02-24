dd4873bfe01f614d110da203a71c047dc76bcfb1
tsetseenn <tsetsenbileg0413@gmail.com>
2021-02-24 20:18:07 +0800

    3

---
+++ spring5webapp/src/main/java/com/tsetsenbileg/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.tsetsenbileg.spring5webapp.repositories;
+
+import com.tsetsenbileg.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
